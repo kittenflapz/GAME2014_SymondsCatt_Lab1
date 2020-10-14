960abf5fd619fba8d6b84ba859966c154330f151
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-10-14 18:59:02 -0400

    Camera follow functionality

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿/* 
+ Filename: CameraFollow.cs
+ Author: Catt Symonds
+ Student Number: 101209214
+ Date Last Modified: 14/10/2020
+ Description: Camera following functionality for camera to follow player
+ Revision History: 
+ 14/10/2020: File created, basic camera follow functionality in place
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.y = (player.transform.position + offset).y;
+        transform.position = position;
+    }
+}
